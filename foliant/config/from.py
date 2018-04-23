589664a71e08f8d58437a88d8f78aea30ef42ee6
Artemy Lomov <lomov@restream.rt.ru>
2018-04-23 18:11:00 +0300

    Add logging.

--- foliant/config/from.py
+++ foliant/config/from.py
@@ -26,12 +26,15 @@ class Parser(BaseParser):
         super().__init__(*args, **kwargs)
 
         self._src_dir_path = Path(self._get_multiproject_config()['src_dir']).expanduser()
+        self.logger = self.logger.getChild('from')
         self._cache_dir_path = self.project_path / '.multiprojectcache'
         self._subproject_config_file_name = self.config_path.name
 
         add_constructor('!from', self._resolve_from_tag)
 
     def _get_multiproject_config(self) -> Dict:
+        self.logger.debug('Getting config of the multiproject')
+
         def _fake_resolve_from_tag(_, node):
             return node.value
 
@@ -41,11 +44,17 @@ class Parser(BaseParser):
             multiproject_config = {**self._defaults, **load(multiproject_config_file)}
             return multiproject_config
 
+        self.logger.debug(f'Config of the multiproject: {multiproject_config}')
+
     def _sync_repo(self, repo_url: str, revision: str or None = None) -> Path:
         repo_name = repo_url.split('/')[-1].rsplit('.', maxsplit=1)[0]
         repo_path = self._cache_dir_path / repo_name
 
+        self.logger.debug(f'Synchronizing repo; URL: {repo_url}, revision: {revision}, name: {repo_name}, local path: {repo_path}')
+
         try:
+            self.logger.debug(f'Cloning repo {repo_url} to {repo_path}')
+
             run(
                 f'git clone {repo_url} {repo_path}',
                 shell=True,
@@ -55,6 +64,8 @@ class Parser(BaseParser):
             )
 
         except CalledProcessError:
+            self.logger.debug('Cloning failed; maybe, repo already cloned; pulling from remote')
+
             run(
                 'git pull',
                 cwd=repo_path,
@@ -65,6 +76,8 @@ class Parser(BaseParser):
             )
 
         if revision:
+            self.logger.debug(f'Checking out to revision: {revision}')
+
             run(
                 f'git checkout {revision}',
                 cwd=repo_path,
@@ -77,6 +90,7 @@ class Parser(BaseParser):
         return repo_path
 
     def _get_chapters_with_overwritten_paths(self, chapters: Dict, subproject_name: str) -> Dict:
+        self.logger.debug('Overwriting paths of chapters')
 
         def _recursive_process_chapters(chapters_subset, subproject_name):
             if isinstance(chapters_subset, dict):
@@ -103,10 +117,14 @@ class Parser(BaseParser):
 
         new_chapters = _recursive_process_chapters(chapters, subproject_name)
 
+        self.logger.debug(f'Chapters with overwritten paths: {new_chapters}')
+
         return new_chapters
 
     def _resolve_from_tag(self, _, node) -> Dict:
         subproject_location = node.value
+
+        self.logger.debug(f'Resolving subproject location: {subproject_location}')
 
         if subproject_location.find('://') >= 0:
             subproject_location_parts = subproject_location.split('#', maxsplit=1)
@@ -116,23 +134,45 @@ class Parser(BaseParser):
             if len(subproject_location_parts) == 2:
                 revision = subproject_location_parts[1]
 
+            self.logger.debug(f'Subproject location is the remote repo: {repo_url}, revision: {revision}')
+
             subproject_dir_path = self._sync_repo(repo_url, revision)
+
+            self.logger.debug(f'Subproject saved to local path: {subproject_dir_path}')
 
         else:
             subproject_dir_path = Path(subproject_location).expanduser()
 
+            self.logger.debug(f'Subproject location is the local path: {subproject_dir_path}')
+
         subproject_config_file_path = subproject_dir_path / self._subproject_config_file_name
+
+        self.logger.debug(f'Subproject config file: {subproject_config_file_path}')
 
         with open(subproject_config_file_path) as subproject_config_file:
             subproject_config = load(subproject_config_file)
+
+        self.logger.debug(f'Subproject config: {subproject_config}')
 
         subproject_title = subproject_config['title']
         subproject_chapters = {subproject_title: subproject_config['chapters']}
 
         subproject_name = subproject_dir_path.name
 
+        self.logger.debug(f'Subproject title: {subproject_title}')
+        self.logger.debug(f'Subproject chapters: {subproject_chapters}')
+        self.logger.debug(f'Subproject name: {subproject_name}')
+
+        self.logger.debug('Calling Foliant to build the subproject')
+
+        multiproject_logger = self.logger
+
         make_module = import_module('foliant.cli.make')
-        preprocessed_subproject_dir_path = Path(make_module.Cli().make(target='pre', project_path=subproject_dir_path))
+        preprocessed_subproject_dir_path = Path(make_module.Cli().make(target='pre', project_path=subproject_dir_path, debug=False))
+
+        self.logger = multiproject_logger
+
+        self.logger.debug(f'Moving built subproject to {self._src_dir_path / subproject_name}')
 
         rmtree(self._src_dir_path / subproject_name, ignore_errors=True)
         move(preprocessed_subproject_dir_path, self._src_dir_path / subproject_name)
--- foliant/preprocessors/repolink.py
+++ foliant/preprocessors/repolink.py
@@ -28,6 +28,10 @@ class Preprocessor(BasePreprocessor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.logger = self.logger.getChild('repolink')
+
+        self.logger.debug(f'Preprocessor inited: {self.__dict__}')
+
     def _add_repo_link(self, markdown_file_relative_path: str, content: str) -> str:
         if self.options['repo_url']:
             repo_url = self.options['repo_url'].rstrip('/')
@@ -35,6 +39,11 @@ class Preprocessor(BasePreprocessor):
             repo_url_with_edit_uri = f'{repo_url}/{edit_uri}'.rstrip('/')
 
             link_html_attributes = self.options['link_html_attributes']
+
+            self.logger.debug(f'Link href: {repo_url_with_edit_uri}/{markdown_file_relative_path}')
+            self.logger.debug(f'Link attributes: {link_html_attributes}')
+            self.logger.debug(f'Link text: {self.options["link_text"]}')
+            self.logger.debug(f'Link title: {self.options["link_title"]}')
 
             if link_html_attributes:
                 link_html_attributes = ' ' + link_html_attributes
@@ -53,6 +62,11 @@ class Preprocessor(BasePreprocessor):
         return content
 
     def apply(self):
+        self.logger.info('Applying preprocessor')
+
+        self.logger.debug(f'Allowed targets: {self.options["targets"]}')
+        self.logger.debug(f'Current target: {self.context["target"]}')
+
         if not self.options['targets'] or self.context['target'] in self.options['targets']:
             for markdown_file_path in self.working_dir.rglob('*.md'):
                 with open(markdown_file_path, encoding='utf8') as markdown_file:
@@ -60,3 +74,5 @@ class Preprocessor(BasePreprocessor):
 
                 with open(markdown_file_path, 'w', encoding='utf8') as markdown_file:
                     markdown_file.write(self._add_repo_link(f'{markdown_file_path.relative_to(self.working_dir)}', content))
+
+        self.logger.info('Preprocessor applied')
