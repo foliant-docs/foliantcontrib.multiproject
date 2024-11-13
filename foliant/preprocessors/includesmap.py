f2b5762de2d3e0473b50de209c9a3d52ce2783d5
Timur Osmanov <timurosm@yandex.ru>
2024-11-13 16:26:20 +0300

    bump version, update changelog, remove trailing spaces

--- foliant/preprocessors/includesmap.py
+++ foliant/preprocessors/includesmap.py
@@ -12,7 +12,7 @@ from foliant.preprocessors.base import BasePreprocessor
 
 class Preprocessor(BasePreprocessor):
     defaults = {}
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.multiproject_includes_map = []
@@ -20,7 +20,7 @@ class Preprocessor(BasePreprocessor):
         self.logger = self.logger.getChild('includesmap')
 
         self.logger.debug(f'Preprocessor inited: {self.__dict__}')
-    
+
     def apply(self):
         self.logger.info('Applying preprocessor')
 
@@ -35,8 +35,7 @@ class Preprocessor(BasePreprocessor):
                     dir = dir.parent
                 obj = json.loads(data)
                 self.multiproject_includes_map.append({f"{dir.as_posix()}": obj})
-        
+
         Path(f'{self.working_dir}/static/').mkdir(parents=True, exist_ok=True)
         with open(f'{self.working_dir}/static/multiproject_includes_map.json', 'w', encoding='utf8') as f:
             json.dump(self.multiproject_includes_map, f)
-        
